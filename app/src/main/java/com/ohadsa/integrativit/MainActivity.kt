09b41d3b9adb170327f0045178d4721dfec738b0
tedooo <ohad@tedooo.com>
2023-01-28 22:49:55 +0200

    init

---
+++ app/src/main/java/com/ohadsa/integrativit/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.ohadsa.integrativit
+
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.activity.viewModels
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.FragmentActivity
+import com.example.integrativit_client.MainViewModel
+import com.ohadsa.a_to_z.R
+import dagger.hilt.android.AndroidEntryPoint
+import net.yslibrary.android.keyboardvisibilityevent.util.UIUtil
+
+
+@AndroidEntryPoint
+class MainActivity : FragmentActivity() {
+
+
+
+    private val viewModel: MainViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+    private var shouldHide = false
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        when (ev?.action) {
+            MotionEvent.ACTION_UP -> {
+                if (shouldHide) {
+                    currentFocus?.let { focus ->
+                        UIUtil.hideKeyboard(this)
+                        focus.clearFocus()
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE -> shouldHide = false
+            MotionEvent.ACTION_DOWN -> shouldHide = true
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
